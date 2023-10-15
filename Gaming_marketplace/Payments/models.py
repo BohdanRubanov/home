37c4832bb3d04a6bc926fd7543fd698b65f5cc42
Anon8388 <146993524+Anon8388@users.noreply.github.com>
2023-10-15 09:13:18 +0300

    home_task

---
+++ Gaming_marketplace/Payments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Game(models.Model):
+    username =  models.CharField(max_length=255)
+    amount_money =  models.IntegerField()
+    comment = models.CharField(max_length=255, null= True, blank=True)
+    
